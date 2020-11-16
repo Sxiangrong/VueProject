f52588c797609bb1cdf28e5342f2acd3b9272420
shixiangrong <7661304+NaCy_cheery@user.noreply.gitee.com>
2020-11-16 10:10:11 +0800

    ’frist

---
+++ ruoyi-ui/src/views/dashboard/NewIndex/MainContext.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <el-row :gutter="32">
+      <el-col :span="4" :xs="24" :sm="24" :lg="8" v-for="(item,index) in cardText" :key="index">
+        <el-popover placement="right-start" width="200" trigger="hover">
+          <template v-if="item.id==1" :class="{'activeDiv':'flag'}">
+            <div><p>观影、春/秋游类</p></div>
+            <div><p>生日蛋糕券</p></div>
+            <div><p>节日福利</p></div>
+            <div><p>恶劣天气防护</p></div>
+            <div><p>文体活动类</p></div>
+            <div><p>劳动竞赛</p></div>
+            <div><p>其他</p></div>
+          </template>
+          <el-card shadow="always" class="box fontText" slot="reference">
+            {{item.content}}
+          </el-card>
+        </el-popover>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="32">
+      <el-col :span="4" :xs="24" :sm="24" :lg="8" v-for="item in cardText1">
+        <el-card shadow="always" class="box fontText">
+          {{item.content}}
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+
+</template>
+
+<script>
+  let cardText=[
+    {
+      id:1,
+      content:"经费登记备案"
+    },{
+      id:2,
+      content:"文件管理"
+    },{
+      id:3,
+      content:"发送通知"
+    }
+  ]
+  let cardText1=[
+  {
+    id:1,
+    content:"基层工会"
+  },{
+    id:2,
+    content:"经费设置"
+  },{
+    id:3,
+    content:"总工会"
+  }
+]
+
+  export default {
+        name: "MainContext",
+        data(){
+          return{
+            cardText:cardText,
+            cardText1:cardText1,
+            showContext:false,
+            flag:true,
+            showText: false,
+            hideText: true
+
+          }
+      },
+
+    methods:{
+       /*isShow :function (event) {
+           let _el=event.currentTarget;
+         if (_el.getAttribute('class') === 'activeDiv') {
+           _el.setAttribute('class', '')
+         } else {
+           _el.setAttribute('class', 'activeDiv')		//如果不存在添加active属性,隐藏该节点
+         }
+
+       }*/
+      overShow () {
+        this.showText = !this.showText;
+        this.hideText = !this.hideText;
+      },
+      outHide () {
+        this.showText = !this.showText;
+        this.hideText = !this.hideText;
+      }
+    }
+
+    }
+</script>
+
+<style scoped>
+ .box{
+   width: 80%;
+   height: 170px;
+   background: inherit;
+   background-color: #80b0e2;
+   display: inline-block;
+   margin: 30px;
+   border-radius: 10px 10px  10px  10px;
+   box-shadow: 8px  5px 5px #979ba3;
+ }
+  .fontText{
+    font-family: "Arial Narrow";
+    font-weight: 700;
+    font-style: normal;
+    font-size: 30px;
+    text-align: center;
+    line-height: 50%;
+    color: whitesmoke;
+    padding-top: 70px;
+  }
+
+  .el-card:hover{
+    transform: scale(1.1,1.1);
+    transition-duration:1s;
+  }
+ .activeDiv{
+  display: inline-block;
+   z-index: 999;
+}
+
+</style>
---
+++ ruoyi-ui/src/views/system/policy/index.vue
@@ -0,0 +1,98 @@
+<template>
+<div class="app-container">
+
+    <el-form :inline="true" :model="formInline" class="el-form--inline el-col-offset-10" >
+      <el-form-item>
+        <el-select  v-model="formInline.formItem" placeholder="请选择申请事项">
+          <el-option label="电影券发放" value="movie"></el-option>
+          <el-option label="组织春秋游" value="trip"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item >
+        <el-select v-model="formInline.formCompany" placeholder="请选择申请单位">
+          <el-option label="天津河西区" value="shanghai"></el-option>
+          <el-option label="天津小学" value="beijing"></el-option>
+          <el-option label="河西区高级附属中学" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item >
+        <el-input v-model="formInline.writeTime" placeholder="填表时间xxxx-xx-xx"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" >查询</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
+      <el-table-column prop="id" label="序号" width="180"></el-table-column>
+    <el-table-column prop="itemEvent" label="申请事项" width="180"></el-table-column>
+    <el-table-column prop="money" label="申请金额（元）" width="180"></el-table-column>
+    <el-table-column prop="peopleAccount" label="人数"></el-table-column>
+      <el-table-column prop="company" label="申请单位"></el-table-column>
+      <el-table-column prop="applyPeople" label="申请人"></el-table-column>
+      <el-table-column prop="writeTime" label="申请时间"></el-table-column>
+      <el-table-column prop="operation" label="操作">
+        <el-button-group>
+          <el-button type="text">详情</el-button>
+          <el-button type="text">同意</el-button>
+          <el-button type="text">驳回</el-button>
+        </el-button-group>
+      </el-table-column>
+  </el-table>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+      data(){
+          return{
+            formInline:{
+              formItem:'',
+              writeTime:'',
+              formCompany:''
+            },
+            tableData: [
+              {id:1,
+                itemEvent: '电影券发放',
+                money: '1000元',
+                peopleAccount: '100',
+                company:'天津河西区',
+                applyPeople:'张三',
+                writeTime:'2020-10-24',
+                operation:''
+            },
+              { id :2,
+                itemEvent: '观影春秋游',
+                money: '1000元',
+                peopleAccount: '100',
+                company:'天津小学',
+                applyPeople:'小明',
+                writeTime:'2020-10-24',
+                operation:''
+              },
+            ]
+          }
+      },
+      methods: {
+        tableRowClassName({row, rowIndex}) {
+          if (rowIndex === 1) {
+            return 'warning-row';
+          } else if (rowIndex === 3) {
+            return 'success-row';
+          }
+          return '';
+        }
+      },
+    }
+</script>
+
+<style scoped>
+  .el-table .warning-row {
+    background: oldlace;
+  }
+
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
+</style>
